1076a0192cac97b44b3e67b002510427baa18df4
Artur Gol. <mastersword@iMac-Artur.lan>
2023-09-14 01:20:24 +0300

    updated with eslint prettier

---
+++ fashio8fusion/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+  return (
+    <div className={styles.home}>
+      <h1>Home</h1>
+    </div>
+  );
+};
